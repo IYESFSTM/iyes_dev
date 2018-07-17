caa31406c0308578b5f89c0b7204c95bb79036e0
Abdelouahid-eddaoudi <40953766+Abdelouahid-eddaoudi@users.noreply.github.com>
2018-07-17 23:59:10 +0100

    Add files via upload

---
+++ iyes_back/src/main/java/uh2/fstm/IyesApplication.java
@@ -0,0 +1,12 @@
+package uh2.fstm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IyesApplication.class, args);
+	}
+}
